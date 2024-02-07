a8713238fe7ff4121e8d6765d82b1e71744c60ef
Nadia Hosseini <nadiahosseini018@gmail.com>
2024-02-07 23:38:45 +0330

    Initial Commit

--- src/main/java/com/demo/mvc/controller/DeveloperController.java
+++ src/main/java/com/demo/mvc/controller/DeveloperController.java
@@ -1,7 +1,9 @@
 package com.demo.mvc.controller;
 
+import com.demo.mvc.dto.DeveloperDto;
 import com.demo.mvc.model.Developer;
 import com.demo.mvc.service.DeveloperService;
+import com.demo.mvc.service.RelDeveloperSkillService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,37 +12,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/developers")
 public class DeveloperController {
     private DeveloperService developerService;
+    private RelDeveloperSkillService relDeveloperSkillService;
 
-    @GetMapping
+    @GetMapping("/")
     public ResponseEntity<List<Developer>> getAllDevelopers() {
         return new ResponseEntity<>(developerService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Optional<Developer> getDeveloperById(@PathVariable Long id) {
-        return developerService.findById(id);
+    public ResponseEntity<Optional<Developer>> getDeveloperById(@PathVariable Long id) {
+        return new ResponseEntity<>(developerService.findById(id), HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
-    public
-
-    @PostMapping
-    public Developer createDeveloper(@RequestBody Developer developer) {
-        return developerService.save(developer);
+    @GetMapping("/developerSkill/{id}")
+    public ResponseEntity<DeveloperDto> getDeveloperSkills(@PathVariable Long id) {
+        return new ResponseEntity<>(relDeveloperSkillService.getDeveloper(id), HttpStatus.OK);
     }
 
-    @PutMapping("/{id}")
-    public Developer updateDeveloper(@PathVariable Long id, @RequestBody Developer developer) {
+    @PostMapping("/save")
+    public ResponseEntity<Developer> saveDeveloper(@RequestBody Developer developer) {
+        return new ResponseEntity<>(developerService.save(developer), HttpStatus.OK);
+    }
+
+    @PostMapping("/saveWithSkills")
+    public ResponseEntity<DeveloperDto> saveDeveloperSkills(@RequestBody DeveloperDto dto) {
+        return new ResponseEntity<>(relDeveloperSkillService.saveDeveloperSkills(dto), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Developer> updateDeveloper(@PathVariable Long id, @RequestBody Developer developer) {
         developer.setId(id);
-        return developerService.save(developer);
+        return new ResponseEntity<>(developerService.save(developer), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteDeveloper(@PathVariable Long id) {
         developerService.deleteById(id);
     }
